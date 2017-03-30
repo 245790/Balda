d3a11705199957a8c4d474fe59f5787b0e47c80a
Karim <karim_karim_1997@mail.ru>
2017-03-30 17:42:34 +0300

    Initial commit

---
+++ Balda/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Balda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createGameButton = new System.Windows.Forms.Button();
+            this.recordsButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // createGameButton
+            // 
+            this.createGameButton.Location = new System.Drawing.Point(168, 94);
+            this.createGameButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.createGameButton.Name = "createGameButton";
+            this.createGameButton.Size = new System.Drawing.Size(264, 42);
+            this.createGameButton.TabIndex = 0;
+            this.createGameButton.Text = "Создать игру";
+            this.createGameButton.UseVisualStyleBackColor = true;
+            // 
+            // recordsButton
+            // 
+            this.recordsButton.Location = new System.Drawing.Point(168, 148);
+            this.recordsButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.recordsButton.Name = "recordsButton";
+            this.recordsButton.Size = new System.Drawing.Size(264, 42);
+            this.recordsButton.TabIndex = 1;
+            this.recordsButton.Text = "Таблица рекордов";
+            this.recordsButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(168, 202);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(264, 42);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Выход";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 383);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.recordsButton);
+            this.Controls.Add(this.createGameButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Компьютерная игра \"Балда\"";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createGameButton;
+        private System.Windows.Forms.Button recordsButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
