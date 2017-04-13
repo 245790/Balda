d50f55ed3aeec9578b8faed72796dabe5d5c05dc
Nastya <denchik.rizhulin@gmail.com>
2017-04-13 12:57:38 +0300

    added class FieldState

---
+++ Balda/FieldState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Balda
+{
+    class FieldState
+    {
+        public char[,] Field { get; set; }
+        public int NewX { get; set; }
+        public int NewY { get; set; }
+        public List<int> X { get; set; }
+        public List<int> Y { get; set; }
+        public FieldState (string startWord)
+        {
+            int wordLen = startWord.Length;
+            Field = new char[wordLen, wordLen];
+            for (int i = 0; i < wordLen; i++)
+            {
+                for (int j = 0; j < wordLen; j++)
+                {
+                    if (i == wordLen / 2 + 1)
+                    {
+                        Field[i, j] = startWord[j];
+                    }
+                    else
+                    {
+                        Field[i, j] = ' ';
+                    }                    
+                }
+            }
+            X = new List<int>();
+            Y = new List<int>();
+            NewX = -1;
+            NewY = -1;
+        }
+    }
+}
